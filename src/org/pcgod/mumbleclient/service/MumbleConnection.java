fca9a1575a43fb18eb443347419ccfb23ec7d561
Mikko Rantanen <jubjub@jubjubnest.net>
2010-11-07 06:06:03 +0200

    Enhanced connection state logging

--- src/org/pcgod/mumbleclient/service/MumbleConnection.java
+++ src/org/pcgod/mumbleclient/service/MumbleConnection.java
@@ -242,6 +242,7 @@ public class MumbleConnection implements Runnable {
 			DatagramSocket udpSocket;
 <<<<<<< HEAD
 
+			Log.i(Globals.LOG_TAG, String.format("Connecting to host \"%s\", port %s", host, port));
 			final SSLContext ctx_ = SSLContext.getInstance("TLS");
 			ctx_.init(
 				null,
@@ -253,10 +254,12 @@ public class MumbleConnection implements Runnable {
 			tcpSocket.setEnabledProtocols(new String[] { "TLSv1" });
 			tcpSocket.startHandshake();
 
+			Log.i(Globals.LOG_TAG, "TCP/SSL socket opened");
+
 			udpSocket = new DatagramSocket();
 			udpSocket.connect(Inet4Address.getByName(host), port);
 
-			Log.i(Globals.LOG_TAG, "Syncing");
+			Log.i(Globals.LOG_TAG, "UDP Socket opened");
 
 			synchronized (stateLock) {
 				connectionHost.setConnectionState(ConnectionState.Synchronizing);
@@ -284,17 +287,19 @@ public class MumbleConnection implements Runnable {
 				udpSocket.close();
 			}
 		} catch (final NoSuchAlgorithmException e) {
-			e.printStackTrace();
+			Log.e(Globals.LOG_TAG, String.format("Could not connect to Mumble server \"%s:%s\"", host, port), e);
 		} catch (final KeyManagementException e) {
-			e.printStackTrace();
+			Log.e(Globals.LOG_TAG, String.format("Could not connect to Mumble server \"%s:%s\"", host, port), e);
 		} catch (final UnknownHostException e) {
 			errorString = String.format("Host \"%s\" unknown", host);
+			Log.e(Globals.LOG_TAG, errorString, e);
 		} catch (final ConnectException e) {
 			errorString = "The host refused connection";
+			Log.e(Globals.LOG_TAG, errorString, e);
 		} catch (final IOException e) {
-			Log.e(Globals.LOG_TAG, "Error connecting", e);
+			Log.e(Globals.LOG_TAG, String.format("Could not connect to Mumble server \"%s:%s\"", host, port), e);
 		} catch (final InterruptedException e) {
-			e.printStackTrace();
+			Log.e(Globals.LOG_TAG, String.format("Could not connect to Mumble server \"%s:%s\"", host, port), e);
 		}
 
 		synchronized (stateLock) {
@@ -309,7 +314,7 @@ public class MumbleConnection implements Runnable {
 		try {
 			sendMessage(MessageType.TextMessage, tmb);
 		} catch (final IOException e) {
-			e.printStackTrace();
+			Log.e(Globals.LOG_TAG, "Could not send text message", e);
 		}
 
 		final Message msg = new Message();
@@ -601,6 +606,7 @@ public class MumbleConnection implements Runnable {
 			errorString = String.format(
 				"Connection rejected: %s",
 				reject.getReason());
+			Log.e(Globals.LOG_TAG, String.format("Received Reject message: %s", reject.getReason()));
 			break;
 		case ServerSync:
 			final ServerSync ss = ServerSync.parseFrom(buffer);
@@ -824,7 +830,7 @@ public class MumbleConnection implements Runnable {
 
 		final User u = findUser((int) uiSession);
 		if (u == null) {
-			Log.e(Globals.LOG_TAG, "User session " + uiSession + "not found!");
+			Log.e(Globals.LOG_TAG, "User session " + uiSession + " not found!");
 		}
 
 		// Rewind the packet. Otherwise consumers are confusing to implement.
